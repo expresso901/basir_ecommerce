f6c606789ab217084c5976ce9d7fe41d143e9904
expresso901 <sotatleung@gmail.com>
2024-01-24 13:34:17 +0800

    01-Create-Next-App-Layout

--- MERN2003/READ.md
+++
@@ -1,7 +0,0 @@
-# TypeScript MERN
-## Lessons
-
-1.  Introduction
-2.  Install Tools
-3.  Create Typescript React App by Vite
-4.  Create Git Repository
---
+++ next-tailwind-ecommerce/app/cart/page.js
@@ -0,0 +1,113 @@
+'use client';
+import { Store } from '@/utils/Store';
+import Link from 'next/link';
+import { useContext } from 'react';
+import Image from 'next/image';
+import { XCircleIcon } from '@heroicons/react/24/outline';
+import { useRouter } from "next/navigation";
+import dynamic from 'next/dynamic';
+
+function CartScreen() {
+
+    const { state, dispatch } = useContext(Store);
+    const router = useRouter();
+    const {
+      cart: { cartItems },
+    } = state;
+
+    const removeItemHandler = (item) => {
+        dispatch({ type: 'CART_REMOVE_ITEM', payload: item});
+    };
+    const updateCartHandler = (item, qty) => {
+      const quantity = Number(qty);
+      dispatch({type:'CART_ADD_ITEM', payload:{...item, quantity}});
+    }
+
+    return (
+      <div>
+        <h1 className="mb-4 text-xl">Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <div>
+            Cart is empty. <Link href="/">Go shopping</Link>
+          </div>
+        ) : (
+          <div className="grid md:grid-cols-4 md:gap-5">
+            <div className="overflow-x-auto md:col-span-3">
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-left">Item</th>
+                    <th className="px-5 text-right">Quantity</th>
+                    <th className="px-5 text-right">Price</th>
+                    <th className="px-5">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cartItems.map((item) => (
+                    <tr key={item.slug} className="border-b">
+                      <td>
+                        <Link
+                          href={`/product/${item.slug}`}
+                          className="flex items-center"
+                        >
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          />
+                          &nbsp;
+                          {item.name}
+                        </Link>
+                      </td>
+                      <td className="p-5 text-right">
+                        <select
+                          value={item.quantity}
+                          onChange={(e) =>
+                            updateCartHandler(item, e.target.value)
+                          }
+                        >
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </select>
+                      </td>
+                      <td className="p-5 text-right">{item.price}</td>
+                      <td className="p-5 text-center">
+                        <button onClick={() => removeItemHandler(item)}>
+                          <XCircleIcon className="h-5 w-5"></XCircleIcon>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="card p-5">
+              <ul>
+                <li>
+                  <div className="pb-3 text-xl">
+                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) :
+                    ${cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+                  </div>
+                </li>
+                <li>
+                  <button
+                    className="primary-button w-full"
+                    onClick={() => router.push('/shipping')}
+                  >
+                    Check Out
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
+    )
+}
+
+// export default dynamic(() => Promise.resolve(CartScreen), {ssr: false})
+export default CartScreen;
