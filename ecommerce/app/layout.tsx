621c9813af8232379f5344e3ec140ddb05aaed29
expresso901 <sotatleung@gmail.com>
2024-08-14 22:39:27 +0800

    9. Cart Page

--- ecommerce/app/layout.tsx
+++ ecommerce/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { Poppins as FontSans } from 'next/font/google'
 import { cn } from '@/lib/utils'
+import { Toaster } from '@/components/ui/toaster'
 import React from 'react'
 
 const fontsSans = FontSans({
@@ -29,6 +30,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   )
