d135852f18872a77c6b6c00fb1dbe87b2f691f84
expresso901 <sotatleung@gmail.com>
2024-08-14 22:15:35 +0800

    8. add to cart

--- ecommerce/lib/validator.ts
+++ ecommerce/lib/validator.ts
@@ -1,10 +1,13 @@
 import * as z from 'zod'
+import { formatNumberWithDecimal } from './utils'
 
+//SIGNIN
 export const signInFormSchema = z.object({
   email: z.string().email().min(3, 'Email must be at least 3 characters'),
   password: z.string().min(3, 'Password must be at least 3 characters'),
 })
 
+//SIGNUP
 export const signUpFormSchema = z
   .object({
     name: z.string().min(3, 'Name must be at least 3 characters'),
@@ -18,3 +21,18 @@ export const signUpFormSchema = z
     message: "Passwords don't match",
     path: ['confirmPassword'],
   })
+
+// CART
+export const cartItemSchema = z.object({
+  productId: z.string().min(1, 'Product is required'),
+  name: z.string().min(1, 'Name is required'),
+  slug: z.string().min(1, 'Slug is required'),
+  qty: z.number().int().nonnegative('Quantity must be a non-negative number'),
+  image: z.string().min(1, 'Image is required'),
+  price: z
+    .number()
+    .refine(
+      (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(value)),
+      'Price must have exactly two decimal places (e.g., 49.99)'
+    ),
+})
--- ecommerce/types/index.ts
+++ ecommerce/types/index.ts
@@ -1,4 +1,11 @@
-import { products } from "@/db/schema"
+import { carts, products } from "@/db/schema"
+import { cartItemSchema } from "@/lib/validator"
 import { InferSelectModel } from "drizzle-orm"
+import { z } from 'zod'
 
+//PRODUCT
 export type Product = InferSelectModel<typeof products>
+
+//CART
+export type CartItem = z.infer<typeof cartItemSchema>
+export type Cart = InferSelectModel<typeof carts>
