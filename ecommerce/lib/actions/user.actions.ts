ca8d443a66e4ad471201d6293fc9eb081373670d
expresso901 <sotatleung@gmail.com>
2024-08-16 11:22:10 +0800

    10. Payment Method

--- ecommerce/lib/actions/user.actions.ts
+++ ecommerce/lib/actions/user.actions.ts
@@ -3,7 +3,7 @@
 import { isRedirectError } from 'next/dist/client/components/redirect'
 
 import { auth, signIn, signOut } from '@/auth'
-import { shippingAddressSchema, signInFormSchema, signUpFormSchema } from '../validator'
+import { paymentMethodSchema, shippingAddressSchema, signInFormSchema, signUpFormSchema } from '../validator'
 import { formatError } from '../utils'
 import { hashSync } from 'bcrypt-ts-edge'
 import db from '@/db/drizzle'
@@ -11,6 +11,7 @@ import { users } from '@/db/schema'
 import { revalidatePath } from 'next/cache'
 import { ShippingAddress } from '@/types'
 import { eq } from 'drizzle-orm'
+import { z } from 'zod'
 
 export async function signInWithCredentials(
   prevState: unknown,
@@ -87,6 +88,30 @@ export async function updateUserAddress(data: ShippingAddress) {
 
     const address = shippingAddressSchema.parse(data)
     await db.update(users).set({ address }).where(eq(users.id, currentUser.id))
+    revalidatePath('/place-order')
+    return {
+      success: true,
+      message: 'User updated successfully',
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
+
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth()
+    const currentUser = await db.query.users.findFirst({
+      where: (users, { eq }) => eq(users.id, session?.user.id!),
+    })
+    if (!currentUser) throw new Error('User not found')
+    const paymentMethod = paymentMethodSchema.parse(data)
+    await db
+      .update(users)
+      .set({ paymentMethod: paymentMethod.type })
+      .where(eq(users.id, currentUser.id))
     revalidatePath('/place-order')
     return {
       success: true,
