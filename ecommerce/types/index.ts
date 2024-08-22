9f23b1518d2eeb4f870321d0472c89da5d36349f
expresso901 <sotatleung@gmail.com>
2024-08-22 13:21:11 +0800

    12. order details

--- ecommerce/types/index.ts
+++ ecommerce/types/index.ts
@@ -1,6 +1,10 @@
-import { carts, products } from "@/db/schema"
-import { cartItemSchema, paymentResultSchema, shippingAddressSchema } from "@/lib/validator"
-import { InferSelectModel } from "drizzle-orm"
+import { carts, orderItems, orders, products } from '@/db/schema'
+import {
+  cartItemSchema,
+  paymentResultSchema,
+  shippingAddressSchema,
+} from '@/lib/validator'
+import { InferSelectModel } from 'drizzle-orm'
 import { z } from 'zod'
 
 //PRODUCT
@@ -13,3 +17,10 @@ export type Cart = InferSelectModel<typeof carts>
 //SHIPPING ADDRESS
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>
 export type PaymentResult = z.infer<typeof paymentResultSchema>
+
+// ORDERS null is added to name and email of user
+export type Order = InferSelectModel<typeof orders> & {
+  orderItems: OrderItem[]
+  user: { name: string | null; email: string | null }
+}
+export type OrderItem = InferSelectModel<typeof orderItems>
